f0cdf723895e0fbb624f1737576ed592bf0f4c43
RangerWolf <yang.rangerwolf@gmail.com>
2024-09-26 23:04:56 +0800

    refactor: refine structure

--- ngxctl/cli.py
+++ ngxctl/cli.py
@@ -7,23 +7,23 @@ import json
 import os.path
 from queue import Queue
 
-from ngxctl.cmds import top
-from ngxctl.cmds import extract
+from ngxctl.cmds import top, misc
 from ngxctl.utils import config_parser  # 假设你有一个配置解析工具
 
 
-@click.group(invoke_without_command=True, context_settings=dict(help_option_names=['-h', '--help']))
+@click.group(invoke_without_command=False,
+             context_settings=dict(help_option_names=['-h', '--help']))
 @click.option('-c', '--conf', default='/etc/nginx/nginx.conf',
               help='Specify the Nginx configuration file. Default is /etc/nginx/nginx.conf.')
-@click.option('--show-vars', is_flag=True,
-              help='List all params that are available for access log.')
-@click.option('--show-logs', is_flag=True,
-              help='List all access log and error log related information.')
+# @click.option('--show-vars', is_flag=True,
+#               help='List all params that are available for access log.')
+# @click.option('--show-logs', is_flag=True,
+#               help='List all access log and error log related information.')
 # @click.option('--follow/--no-follow', default=True,
 #               help='Follow new lines in the log, similar to tail -f command.')
 @click.option('--debug', is_flag=True, default=False)
 @click.pass_context
-def cli(ctx, conf, show_vars, show_logs, debug):
+def cli(ctx, conf, debug):
     """ngxctl: A command-line tool for managing and analyzing Nginx."""
 
     # ensure that ctx.obj exists and is a dict (in case `cli()` is called
@@ -53,30 +53,30 @@ def cli(ctx, conf, show_vars, show_logs, debug):
     ctx.obj['log_path_results'] = log_path_results
     ctx.obj['log_format_results'] = log_format_results
 
-    if show_vars:
-        variables = config_parser.get_log_format_used_fields(log_path_results, log_format_results)
-        table_data = [[item] for item in variables]
-        print(tabulate.tabulate(table_data, headers=['Variables'], tablefmt='orgtbl'))
-        ctx.exit()  # 退出程序
-
-    if show_logs:
-        table_data = []
-        headers = ['server_name', 'file_name', 'log_path']
-        for item in log_path_results:
-            if item['log_type'] == 'access_log':
-                row = []
-                for header in headers:
-                    if header == 'log_path':
-                        row.append(
-                            item.get('log_args')[0]
-                        )
-                    else:
-                        row.append(
-                            item.get(header, '')
-                        )
-                table_data.append(row)
-        print(tabulate.tabulate(table_data, headers=headers, tablefmt='orgtbl'))
-        ctx.exit()  # 退出程序
+    # if show_vars:
+    #     variables = config_parser.get_log_format_used_fields(log_path_results, log_format_results)
+    #     table_data = [[item] for item in variables]
+    #     print(tabulate.tabulate(table_data, headers=['Variables'], tablefmt='orgtbl'))
+    #     ctx.exit()  # 退出程序
+    #
+    # if show_logs:
+    #     table_data = []
+    #     headers = ['server_name', 'file_name', 'log_path']
+    #     for item in log_path_results:
+    #         if item['log_type'] == 'access_log':
+    #             row = []
+    #             for header in headers:
+    #                 if header == 'log_path':
+    #                     row.append(
+    #                         item.get('log_args')[0]
+    #                     )
+    #                 else:
+    #                     row.append(
+    #                         item.get(header, '')
+    #                     )
+    #             table_data.append(row)
+    #     print(tabulate.tabulate(table_data, headers=headers, tablefmt='orgtbl'))
+    #     ctx.exit()  # 退出程序
 
 
 # 默认回调函数，目前没啥用
@@ -93,6 +93,8 @@ def cli(ctx, conf, show_vars, show_logs, debug):
 
 # Add subcommands to the cli group
 cli.add_command(top.top)
+cli.add_command(misc.vars)
+cli.add_command(misc.files)
 
 # extract的功能暂时还没想清楚怎么使用，先暂停
 # cli.add_command(extract.extract)
---
+++ ngxctl/cmds/misc.py
@@ -0,0 +1,52 @@
+# ngxctl/cmds/extract.py
+
+import click
+import json
+import re
+
+import tabulate
+
+from ngxctl.utils import config_parser
+
+
+@click.command()
+@click.pass_context
+def vars(ctx):
+    """Read nginx conf and show all variables access logs used"""
+    log_path_results = ctx.obj['log_path_results']
+    log_format_results = ctx.obj['log_format_results']
+
+    variables = config_parser.get_log_format_used_fields(log_path_results, log_format_results)
+    table_data = [[item] for item in variables]
+    print(tabulate.tabulate(table_data, headers=['Variables'], tablefmt='orgtbl'))
+    ctx.exit()  # 退出程序
+
+
+@click.command()
+@click.pass_context
+def files(ctx):
+    """Read nginx conf and show all variables access logs used"""
+    log_path_results = ctx.obj['log_path_results']
+    log_format_results = ctx.obj['log_format_results']
+
+    table_data = []
+    headers = ['server_name', 'file_name', 'log_path']
+    for item in log_path_results:
+        if item['log_type'] == 'access_log':
+            row = []
+            for header in headers:
+                if header == 'log_path':
+                    row.append(
+                        item.get('log_args')[0]
+                    )
+                else:
+                    row.append(
+                        item.get(header, '')
+                    )
+            table_data.append(row)
+    print(tabulate.tabulate(table_data, headers=headers, tablefmt='orgtbl'))
+    ctx.exit()  # 退出程序
+
+
+if __name__ == "__main__":
+    vars()
