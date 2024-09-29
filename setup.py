50ebb35185f49cdd8435db0c070af6492cba3be0
rangerwolf <rangerwolf@aliyun.com>
2024-09-29 14:14:16 +0800

    fix encoding

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     author="RangerWolf",
     author_email="yang.rangerwolf@gmail.com",
     description="A command-line tool for managing and analyzing Nginx.",
-    long_description=open('README.md').read(),
+    long_description=open('README.md', encoding="utf-8").read(),
     long_description_content_type='text/markdown',
     #url="https://github.com/yourusername/yourprojectname",  github not ready now~
     classifiers=[
