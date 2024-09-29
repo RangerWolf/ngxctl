64fc5a09c7df9f49c51c39dce93d5a8c9cd77244
rangerwolf <13515117373@163.com>
2024-09-29 19:02:44 +0800

    fix upload script

--- upload_pypi.sh
+++ upload_pypi.sh
@@ -1,3 +1,5 @@
+mkdir -p dist
+rm -rf dist/*
 python setup.py sdist build
 pip install twine --index-url https://mirrors.cloud.tencent.com/pypi/simple --trusted-host mirrors.cloud.tencent.com
 twine upload dist/*
